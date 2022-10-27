d8f0580ff29b8c015241e2a088b5c263cb4f1614
abdhamid937 <abdhamid937@gmail.com>
2022-10-27 14:41:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-    // if(function_exists($_GET['function'])) {
-    //         $_GET['function']();
-    //     }   
+     if(function_exists($_GET['function'])) {
+             $_GET['function']();
+         }   
 
-    // function get_id()
-    // {
-    // $response = array(
-    //                 'status' => 1,
-    //                 'message' =>'Success',
-    //                 'data' => 'ok'
-    //             );               
-    // header('Content-Type: application/json');
-    // echo json_encode($response);
+    function get_id()
+    {
+     $response = array(
+                     'status' => 1,
+                     'message' =>'Success',
+                     'data' => 'ok'
+                 );               
+     header('Content-Type: application/json');
+     echo json_encode($response);
 
-    // }
-    echo "hsdjbj";
+    }
+    //echo "hsdjbj";
 ?>
